b0bb8e25b4cb035b85892b39a6092dde62e005c0
Nic Yeeles <n.yeeles@gmail.com>
2014-06-16 15:44:04 +0100

    Makes search work

--- fluential.rb
+++ fluential.rb
@@ -5,5 +5,23 @@ get '/' do
 end
 
 post '/' do
-	redirect :sports
+	search_term = params[:cat] 
+	@influences = HandleSearch.search(search_term)
+	erb :index
 end
+
+get '/new/:person' do 
+	"hello " + params[:person]
+end
+
+get '/new' do
+	"hello 2 " + params[:person]
+end
+
+
+
+
+
+
+
+
